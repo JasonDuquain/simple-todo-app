17b4fc165ad02e9e0d020e16af6065154b93032e
Jason Duquain <jduquain@gmail.com>
2020-10-12 09:46:02 -0400

    Updates

---
+++ precache-manifest.132a370a803a2f11e90e402760085fab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b50717e307fa401dfa78d9ce9c9399",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "04787c4db03f26db5a25",
+    "url": "/simple-todo-app/static/css/main.b403652e.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04787c4db03f26db5a25",
+    "url": "/simple-todo-app/static/js/main.8590908f.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
